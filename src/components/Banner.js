f18a97787398844ed2f61d232c54016935e40fa2
Michael <tharratt.michael@gmail.com>
2023-04-21 12:09:56 +1000

    banner

---
+++ src/components/Banner.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { auth } from "../.firebase";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+
+export default function Banner() {
+    const [user] = useAuthState(auth);
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithRedirect(auth, provider);
+      };
+
+    const signOut = () => {
+    auth.signOut();
+  };
+
+ 
+    
+  return (
+    <header>
+        <h1>React Chat</h1>
+      {user ? (
+        <button onClick={signOut} className="sign-out" type="button">
+          Sign Out
+        </button>
+      ) : (
+        <button onClick={googleSignIn} className="sign-in">Sign In</button>
+      )}
+    </header>
+    
+  )
+}
