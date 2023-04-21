1e33fd2d9d02c6200e242132ef1033a017addd80
Michael <tharratt.michael@gmail.com>
2023-04-21 12:16:25 +1000

    fixing envir variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { auth } from "./.firebase";
+import { useEffect } from 'react';
+import { auth } from "./firebase";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import Banner from './components/Banner'
-import { useEffect } from 'react';
+
 
 function App() {
   const [user] = useAuthState(auth);
