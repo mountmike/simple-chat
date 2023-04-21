f16a22c79edce23ec0a9074a067401e4e30d53a3
Michael <tharratt.michael@gmail.com>
2023-04-21 14:35:55 +1000

    more css

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,13 +1,15 @@
 import "./Message.css"
 
 export default function Message({message}) {
+    console.log(message);
     return (
         <div className="Message">
-            <section className="User">
-                {message.name}
+            <section className="left">
+                <span className="message-name">{message.name}</span>
+                <span>{message.text}</span>
             </section>
-            <section className="Messages">
-                {message.text}
+            <section className="right">
+               <span className="time-sent">{message.createdAt.seconds}</span>
             </section>
         </div>
     )
