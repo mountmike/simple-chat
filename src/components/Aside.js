6127d0efb2269443e4430bd2760df43f1d938f16
Michael <tharratt.michael@gmail.com>
2023-04-21 16:45:37 +1000

    building out aside UI

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,7 +1,35 @@
+import "./Aside.css"
+import { auth } from "../firebase";
+import ChatCard from './ChatCard'
+import { useState } from "react";
+
 export default function Aside() {
+    const { uid, displayName, photoURL } = auth.currentUser;
     return (
         <aside className="Aside">
-            <p>aside</p>
+            <header>
+                <section className="currentUser">
+                    <img className="display-img" src={photoURL} alt="" />
+                    <h4 className="display-name">{displayName}</h4>
+                </section>
+                <button>new chat</button>
+            </header>
+            <section className="search-bar">
+                <input placeholder="Search" type="text" name="" id="" />
+            </section>
+            <section className="chat-list">
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+                <ChatCard />
+            </section>
         </aside>
     )
 }
