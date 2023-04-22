dbca9b666a58bb7ad693acfa4e3677fcc5d724a4
squshedfrog <simonruc@yahoo.com.au>
2023-04-22 10:08:10 +1000

    moved userRegistering

--- src/App.js
+++ src/App.js
@@ -4,11 +4,38 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import Banner from './components/Banner'
+import { useEffect, useState } from 'react';
+import { GetUsers ,RegisterUser } from './user';
+import { query, collection, orderBy, onSnapshot, limit} from "firebase/firestore";
+import { db } from './firebase';
+
+let currentUser
 
 function App() {
+  
+  const [ users , setUsers ] = useState()
+  //login user
   const [user] = useAuthState(auth);
- 
+  // data base user  
+
+  
+  useEffect(()=> {
+    GetUsers(setUsers)
+    },[user])
     
+  if(users){
+    if(!users.map(user => (user.id)).includes(user.uid)){
+      RegisterUser()
+      }
+    
+    }
+  
+
+  
+          
+
+
+
   
   
   return (
